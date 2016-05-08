b810b3209152d3740d1818bfe7482d83c7abd634
McKorleone <paul.koerner@edu.fh-joanneum.at>
2016-05-08 21:08:20 +0200

    create NotesModel; modified UserModel

---
+++ src/at/fh/swenga/samt/model/NotesModel.java
@@ -0,0 +1,76 @@
+package at.fh.swenga.samt.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+@Entity
+@Table(name = "notes")
+public class NotesModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = true, length = 500)
+	private String content;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	private UserModel users;
+
+	@Version
+	long version;
+
+	public NotesModel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public NotesModel(int id, String name, String content) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.content = content;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public UserModel getUsers() {
+		return users;
+	}
+
+	public void setUsers(UserModel users) {
+		this.users = users;
+	}
+
+}
